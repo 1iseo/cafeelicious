e8ae0fcf97b042ab645d8a926fb9288c5441a959
1iseo <96890370+1iseo@users.noreply.github.com>
2025-03-30 13:03:24 +0700

    dont show splash screen for now

--- cafeelicious_mobile/lib/main.dart
+++ cafeelicious_mobile/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cafeelicious_mobile/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: LoginScreen(),
     );
   }
 }
